b00ff65f9eb62e2b6a2360fffd0fb096ed6f68f4
seeaya <connorbarnesdenver@gmail.com>
2021-06-17 13:01:37 -0600

    Initial Commit

---
+++ Fifi/FifiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FifiApp.swift
+//  Fifi
+//
+//  Created by Connor Barnes on 6/17/21.
+//
+
+import SwiftUI
+
+@main
+struct FifiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
