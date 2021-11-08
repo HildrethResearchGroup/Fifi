9bd939f74c07436cedff1cfa04a177870a45e7d6
seeaya <connorbarnesdenver@icloud.com>
2021-11-07 23:42:56 -0700

    Restructured FifiApp.swift

--- Fifi/FifiApp.swift
+++ Fifi/FifiApp.swift
@@ -45,45 +45,7 @@ struct FifiApp: App {
         }
     }
     .commands {
-      CommandGroup(before: .windowList) {
-        ForEach(0..<OpenWindows.allCases.count) { index in
-          let window = OpenWindows.allCases[index]
-          
-          if let digit = Character(String(index)) {
-            Button(window.title) {
-              window.open()
-            }
-            .keyboardShortcut(KeyboardShortcut(KeyEquivalent(digit), modifiers: [.command, .shift]))
-          } else {
-            Button(window.title) {
-              window.open()
-            }
-          }
-        }
-        
-        Divider()
-      }
-      
-			CommandGroup(replacing: .newItem) {
-				Button("New…") {
-					print("New...")
-				}
-				.keyboardShortcut(KeyboardShortcut(KeyEquivalent("n"), modifiers: [.command]))
-				.disabled(true)
-				
-				Button("Open…") {
-					open()
-				}
-				.keyboardShortcut(KeyboardShortcut(KeyEquivalent("o"), modifiers: [.command]))
-				.disabled(printerController.printerQueueState.isRunning)
-				
-				Divider()
-				
-				Button("Save") {
-					save()
-				}
-				.keyboardShortcut(KeyboardShortcut(KeyEquivalent("s"), modifiers: [.command]))
-			}
+      commands()
     }
     .handlesExternalEvents(matching: ["mainWindow"])
     
@@ -97,51 +59,6 @@ struct FifiApp: App {
         .frame(width: 240, height: 360, alignment: .top)
     }
   }
-}
-
-// MARK: Helpers
-extension FifiApp {
-	@MainActor
-	func save() {
-		let panel = NSSavePanel()
-		panel.allowedContentTypes = [.json]
-		panel.canCreateDirectories = true
-		panel.isExtensionHidden = false
-		panel.allowsOtherFileTypes = false
-		panel.title = "Save Operation Queue…"
-		
-		let response = panel.runModal()
-		guard let url = response == .OK ? panel.url : nil else { return }
-		do {
-			let data = try JSONEncoder().encode(printerController.printerQueueState.queue)
-			try data.write(to: url)
-		} catch {
-			print("Could not save")
-		}
-	}
-	
-	@MainActor
-	func open() {
-		let panel = NSOpenPanel()
-		panel.allowedContentTypes = [.json]
-		panel.allowsMultipleSelection = false
-		panel.canChooseDirectories = false
-		panel.canChooseFiles = true
-		
-		let response = panel.runModal()
-		guard let url = response == .OK ? panel.url : nil else { return }
-		do {
-			let data = try Data(contentsOf: url)
-			let decoded = try JSONDecoder().decode(Array<AnyPrinterOperation>.self, from: data)
-			printerController.printerQueueState.queue = decoded
-		} catch {
-			print("Could not open")
-		}
-	}
-	
-	func new() {
-		
-	}
 }
 
 // MARK: Open Windows
