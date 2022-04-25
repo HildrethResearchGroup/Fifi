8fbfe719c7a674fc408e4e9787dc43da37049970
Owen Hildreth <ohildreth@mines.edu>
2022-04-25 13:31:43 -0600

    Got Drag and Drop Working!!!

--- Fifi/Views/OperationQueueView.swift
+++ Fifi/Views/OperationQueueView.swift
@@ -10,6 +10,8 @@ import PrinterController
 
 struct OperationQueueView: View {
   @EnvironmentObject private var printerController: PrinterController
+    
+    @State var selection: Set<UUID> = []
   
   var body: some View {
     VStack {
@@ -19,21 +21,28 @@ struct OperationQueueView: View {
 //				PrinterOperationView(operation: queueState.queue[index], operationIndex: index)
 //			}
 //			.listStyle(.plain)
-			
-			List(queueState.queue) { $operation in
-				PrinterOperationView(
-					operation: $operation,
-					operationIndex: queueState.queue.wrappedValue.firstIndex(of: operation)!
-				)
-					.textFieldStyle(.automatic)
-					.padding(4)
-					.border(Color.secondary)
-					.listRowInsets(EdgeInsets(top: 2, leading: 0, bottom: 0, trailing: 0))
-			}
-			.listStyle(.plain)
-		}
+        List(selection: $selection) {
+            ForEach(queueState.queue) { $operation in
+                PrinterOperationView(
+                    operation: $operation,
+                    operationIndex: queueState.queue.wrappedValue.firstIndex(of: operation)!
+                )
+                .textFieldStyle(.automatic)
+                .padding(4)
+                .border(Color.secondary)
+                .listRowInsets(EdgeInsets(top: 2, leading: 0, bottom: 0, trailing: 0))
+            }
+            .onMove(perform: moveItemsAt(offsets:toOffset:))
+            .listStyle(.plain)
+        }
+        
+    }
 		.disabled(printerController.printerQueueState.isRunning)
   }
+    
+    func moveItemsAt(offsets: IndexSet, toOffset: Int) {
+        queueState.wrappedValue.queue.move(fromOffsets: offsets, toOffset: toOffset)
+    }
 }
 
 // MARK: - Helpers
