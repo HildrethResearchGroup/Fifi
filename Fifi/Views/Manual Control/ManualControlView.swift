d3ad563b422649b569cf73dd1a52ae3933b12eda
Connor Barnes <connorbarnesdenver@icloud.com>
2022-06-13 12:32:40 -0600

    Formatting, added turn on voltage button.

--- Fifi/Views/Manual Control/ManualControlView.swift
+++ Fifi/Views/Manual Control/ManualControlView.swift
@@ -10,155 +10,161 @@ import PrinterController
 import SeeayaUI
 
 struct ManualControlView: View {
-    @EnvironmentObject private var printerController: PrinterController
-    
-    @State private var xJogLocation = 0.0
-    @State private var yJogLocation = 0.0
-    @State private var zJogLocation = 0.0
-    
-    @State private var xMoveLocation = 0.0
-    @State private var yMoveLocation = 0.0
-    @State private var zMoveLocation = 0.0
-    
-    
-   
-    
-    var body: some View {
-        VStack {
-            ManualStageControlView()
-            Divider()
-            ManualElectronicsControl()
-            Divider()
-            SelectableCameraView()
-        }// END: Top Level VStack
-    }
+	@EnvironmentObject private var printerController: PrinterController
+	
+	@State private var xJogLocation = 0.0
+	@State private var yJogLocation = 0.0
+	@State private var zJogLocation = 0.0
+	
+	@State private var xMoveLocation = 0.0
+	@State private var yMoveLocation = 0.0
+	@State private var zMoveLocation = 0.0
+	
+	
+	
+	
+	var body: some View {
+		VStack {
+			ManualStageControlView()
+			Divider()
+			ManualElectronicsControl()
+			Divider()
+			SelectableCameraView()
+		}// END: Top Level VStack
+	}
 }
 
 
 // MARK: Manual Stage Control
 private extension ManualControlView {
-    
-    @ViewBuilder
-    func ManualStageControlView() -> some View {
-        ZStack {
-            Text("Manual Stage Control")
-                .font(.title3)
-            
-            HStack {
-                Spacer()
-                Button(movesHaveBeenAborted ? "Reenable Movement" : "Abort Move") {
-                    if movesHaveBeenAborted {
-                        Task {
-                            try await printerController.reenableMovement()
-                        }
-                    } else {
-                        Task {
-                            try await printerController.abortAllMoves()
-                        }
-                    }
-                }
-                .foregroundColor(.red)
-                .disabled(
-                    printerController.xpsq8State.groupStatus != .moving
-                    && printerController.xpsq8State.groupStatus != .readyDueToAbortMove
-                )
-            }
-        }
-        Text(stageStatusString)
-        
-        ForEach(PrinterController.Dimension.allCases, id: \.rawValue) { dimension in
-            ManualStageView(dimension: dimension, jogLocation: jogLocation(for: dimension), moveLocation: moveLocation(for: dimension), displacementMode: displacementMode(for: dimension))
-        }
-    }
+	
+	@ViewBuilder
+	func ManualStageControlView() -> some View {
+		ZStack {
+			Text("Manual Stage Control")
+				.font(.title3)
+			
+			HStack {
+				Spacer()
+				Button(movesHaveBeenAborted ? "Reenable Movement" : "Abort Move") {
+					if movesHaveBeenAborted {
+						Task {
+							try await printerController.reenableMovement()
+						}
+					} else {
+						Task {
+							try await printerController.abortAllMoves()
+						}
+					}
+				}
+				.foregroundColor(.red)
+				.disabled(
+					printerController.xpsq8State.groupStatus != .moving
+					&& printerController.xpsq8State.groupStatus != .readyDueToAbortMove
+				)
+			}
+		}
+		Text(stageStatusString)
+		
+		ForEach(PrinterController.Dimension.allCases, id: \.rawValue) { dimension in
+			ManualStageView(dimension: dimension, jogLocation: jogLocation(for: dimension), moveLocation: moveLocation(for: dimension), displacementMode: displacementMode(for: dimension))
+		}
+	}
 }
 
 
 // MARK: Manual Waveform Control
 private extension ManualControlView {
-    @ViewBuilder
-    func ManualElectronicsControl() -> some View {
-        ZStack {
-            Text("Manual Voltage Control")
-                .font(.title3)
-            
-            HStack {
-                Spacer()
-                Button("Turn Off Voltage") {
-                    Task {
-                        try await printerController.turnVoltageOff()
-                    }
-                }
-                .foregroundColor(.red)
-            }
-        }
-        
-        ManualVoltageControlView()
-        
-        Text("Manual Multimeter Control")
-            .font(.title3)
-        
-        HStack {
-            ManualMultimeterView()
-            Spacer()
-        }
-    }
+	@ViewBuilder
+	func ManualElectronicsControl() -> some View {
+		ZStack {
+			Text("Manual Voltage Control")
+				.font(.title3)
+			
+			HStack {
+				Spacer()
+				Button("Turn ON Voltage") {
+					Task {
+						try await printerController.turnVoltageOn()
+					}
+				}
+				.foregroundColor(.blue)
+				Button("Turn Off Voltage") {
+					Task {
+						try await printerController.turnVoltageOff()
+					}
+				}
+				.foregroundColor(.red)
+			}
+		}
+		
+		ManualVoltageControlView()
+		
+		Text("Manual Multimeter Control")
+			.font(.title3)
+		
+		HStack {
+			ManualMultimeterView()
+			Spacer()
+		}
+	}
 }
 
 
 
 // MARK: Helpers
 private extension ManualControlView {
-    var movesHaveBeenAborted: Bool {
-        printerController.xpsq8State.groupStatus == .readyDueToAbortMove
-    }
-    
-    var stageStatusString: String {
-        if let status = printerController.xpsq8State.groupStatus {
-            return "\(status) (\(status.rawValue))"
-        } else {
-            return "?"
-        }
-    }
-    
-    func jogLocation(for dimension: PrinterController.Dimension) -> Binding<Double> {
-        switch dimension {
-        case .x:
-            return $xJogLocation
-        case .y:
-            return $yJogLocation
-        case .z:
-            return $zJogLocation
-        }
-    }
-    
-    func moveLocation(for dimension: PrinterController.Dimension) -> Binding<Double> {
-        switch dimension {
-        case .x:
-            return $xMoveLocation
-        case .y:
-            return $yMoveLocation
-        case .z:
-            return $zMoveLocation
-        }
-    }
-    
-    func displacementMode(for dimension: PrinterController.Dimension) -> Binding<DisplacementMode?> {
-        switch dimension {
-        case .x:
-            return $printerController.xpsq8State.xDisplacementMode
-        case .y:
-            return $printerController.xpsq8State.yDisplacementMode
-        case .z:
-            return $printerController.xpsq8State.zDisplacementMode
-        }
-    }
+	var movesHaveBeenAborted: Bool {
+		printerController.xpsq8State.groupStatus == .readyDueToAbortMove
+	}
+	
+	var stageStatusString: String {
+		if let status = printerController.xpsq8State.groupStatus {
+			return "\(status) (\(status.rawValue))"
+		} else {
+			return "?"
+		}
+	}
+	
+	func jogLocation(for dimension: PrinterController.Dimension) -> Binding<Double> {
+		switch dimension {
+		case .x:
+			return $xJogLocation
+		case .y:
+			return $yJogLocation
+		case .z:
+			return $zJogLocation
+		}
+	}
+	
+	func moveLocation(for dimension: PrinterController.Dimension) -> Binding<Double> {
+		switch dimension {
+		case .x:
+			return $xMoveLocation
+		case .y:
+			return $yMoveLocation
+		case .z:
+			return $zMoveLocation
+		}
+	}
+	
+	func displacementMode(for dimension: PrinterController.Dimension) -> Binding<DisplacementMode?> {
+		switch dimension {
+		case .x:
+			return $printerController.xpsq8State.xDisplacementMode
+		case .y:
+			return $printerController.xpsq8State.yDisplacementMode
+		case .z:
+			return $printerController.xpsq8State.zDisplacementMode
+		}
+	}
 }
 
 // MARK: Previews
 struct ManualControlView_Previews: PreviewProvider {
-    static var previews: some View {
-        ManualControlView()
-            .padding()
-            .environmentObject(PrinterController.staticPreview)
-    }
+	static var previews: some View {
+		ManualControlView()
+			.padding()
+			.environmentObject(PrinterController.staticPreview)
+	}
 }
--- Fifi/Views/OperationQueueView.swift
+++ Fifi/Views/OperationQueueView.swift
@@ -9,83 +9,83 @@ import SwiftUI
 import PrinterController
 
 struct OperationQueueView: View {
-  @EnvironmentObject private var printerController: PrinterController
-    
-    @State var selection: Set<UUID> = []
-  
-  var body: some View {
-    VStack {
-      heading
+	@EnvironmentObject private var printerController: PrinterController
+	
+	@State var selection: Set<UUID> = []
+	
+	var body: some View {
+		VStack {
+			heading
 			
-//			List(queueState.queue.indices, id: \.self) { index in
-//				PrinterOperationView(operation: queueState.queue[index], operationIndex: index)
-//			}
-//			.listStyle(.plain)
-        List(selection: $selection) {
-            ForEach(queueState.queue) { $operation in
-                PrinterOperationView(
-                    operation: $operation,
-                    operationIndex: queueState.queue.wrappedValue.firstIndex(of: operation)!
-                )
-                .textFieldStyle(.automatic)
-                .padding(4)
-                .border(Color.secondary)
-                .listRowInsets(EdgeInsets(top: 2, leading: 0, bottom: 0, trailing: 0))
-            }
-            .onMove(perform: moveItemsAt(offsets:toOffset:))
-            .listStyle(.plain)
-        }
-        
-    }
+			//			List(queueState.queue.indices, id: \.self) { index in
+			//				PrinterOperationView(operation: queueState.queue[index], operationIndex: index)
+			//			}
+			//			.listStyle(.plain)
+			List(selection: $selection) {
+				ForEach(queueState.queue) { $operation in
+					PrinterOperationView(
+						operation: $operation,
+						operationIndex: queueState.queue.wrappedValue.firstIndex(of: operation)!
+					)
+						.textFieldStyle(.automatic)
+						.padding(4)
+						.border(Color.secondary)
+						.listRowInsets(EdgeInsets(top: 2, leading: 0, bottom: 0, trailing: 0))
+				}
+				.onMove(perform: moveItemsAt(offsets:toOffset:))
+				.listStyle(.plain)
+			}
+			
+		}
 		.disabled(printerController.printerQueueState.isRunning)
-  }
-    
-    func moveItemsAt(offsets: IndexSet, toOffset: Int) {
-        queueState.wrappedValue.queue.move(fromOffsets: offsets, toOffset: toOffset)
-    }
+	}
+	
+	func moveItemsAt(offsets: IndexSet, toOffset: Int) {
+		queueState.wrappedValue.queue.move(fromOffsets: offsets, toOffset: toOffset)
+	}
 }
 
 // MARK: - Helpers
 private extension OperationQueueView {
-  var queueState: Binding<PrinterQueueState> {
-    $printerController.printerQueueState
-  }
+	var queueState: Binding<PrinterQueueState> {
+		$printerController.printerQueueState
+	}
 }
 
 // MARK: - Subviews
 private extension OperationQueueView {
-  var heading: some View {
-    HStack {
-      Text("Operation Queue")
-        .font(.title3)
-      Spacer()
-      Menu {
-        ForEach(0..<AnyPrinterOperation.allEmptyOperations.count) { index in
-          let templateOperation = AnyPrinterOperation.allEmptyOperations[index]
-          
-          Button() {
-            queueState.queue.wrappedValue.append(templateOperation)
-          } label: {
-            HStack {
-              Image(systemName: templateOperation.thumbnailName)
-              Text(templateOperation.name)
-            }
-          }
-        }
-      } label: {
-        Image(systemName: "plus")
-      }
-      .menuStyle(.borderlessButton)
-      .fixedSize()
-    }
-  }
+	var heading: some View {
+		HStack {
+			Text("Operation Queue")
+				.font(.title3)
+			Spacer()
+			Menu {
+				ForEach(0..<AnyPrinterOperation.allEmptyOperations.count) { index in
+					let templateOperation = AnyPrinterOperation.allEmptyOperations[index]
+					
+					Button() {
+						queueState.queue.wrappedValue.append(templateOperation)
+					} label: {
+						HStack {
+							Image(systemName: templateOperation.thumbnailName)
+							Text(templateOperation.name)
+						}
+					}
+				}
+			} label: {
+				Image(systemName: "plus")
+			}
+			.menuStyle(.borderlessButton)
+			.fixedSize()
+		}
+	}
 }
 
 // MARK: - Previews
 struct OperationQueueView_Previews: PreviewProvider {
-  static var previews: some View {
-    OperationQueueView()
+	static var previews: some View {
+		OperationQueueView()
 			.environmentObject(PrinterController.staticPreview)
-      .padding()
-  }
+			.padding()
+	}
 }
