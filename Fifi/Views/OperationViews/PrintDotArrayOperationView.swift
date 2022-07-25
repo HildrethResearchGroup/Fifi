9d5bfffbd740f179846a0cf23211ab3c9d4a4293
Collin Miller <collinmiller@mines.edu>
2022-07-25 15:20:25 -0600

    Added to Print dot Array functionality
    changed print line so that voltage does not turn off after the first layer

--- Fifi/Views/OperationViews/PrintDotArrayOperationView.swift
+++ Fifi/Views/OperationViews/PrintDotArrayOperationView.swift
@@ -14,11 +14,60 @@ struct PrintDotArrayOperationView: View {
 	
 	var body: some View {
 		VStack {
-			ValidatingTextField("Spacing", value: $configuration.spacing) { value in
-				String(value)
-			} validate: { string in
-				Double(string)
-			}
+            HStack(){
+                Text("Spacing")
+                TextField("Spacing", value: $configuration.spacing, format: appDefaultTextFieldNumberFormatter(3))
+                    .appDefaultTextFieldStyle()
+            }
+           
+            HStack() {
+                Text("Voltage")
+                TextField("Voltage", value: $configuration.voltage, format: appDefaultTextFieldNumberFormatter(3))
+                    .appDefaultTextFieldStyle()
+            }
+            
+            HStack() {
+                Text("Voltage Time")
+                TextField("VoltageTime", value: $configuration.voltageTime, format: appDefaultTextFieldNumberFormatter(3))
+                    .appDefaultTextFieldStyle()
+            }
+            
+           
+            HStack() {
+                Text("Number of X Dots")
+                TextField("Number of Dots", value: $configuration.numberOfXDots, format: .number)
+                    .appDefaultTextFieldStyle()
+            }
+            
+            HStack() {
+                Text("Number of Y Dots")
+                TextField("Number of Dots", value: $configuration.numberOfYDots, format: .number)
+                    .appDefaultTextFieldStyle()
+            }
+            
+            HStack() {
+                Text("Number of Layers")
+                TextField("Number of Layers", value: $configuration.numberOfLayers, format: .number)
+                    .appDefaultTextFieldStyle()
+            }
+            
+            
+            /*
+
+             
+             public var spacing: Double = 1.0
+             public var voltage: Double = 1.0
+             public var voltageTime: Double = 0.1
+             public var numberOfDots: Int = 1
+             public var numberOfLayers: Int = 1
+             
+             ValidatingTextField("Spacing", value: $configuration.spacing) { value in
+                 String(value)
+             } validate: { string in
+                 Double(string)
+             }
+             */
+			
 		}
 	}
 }
