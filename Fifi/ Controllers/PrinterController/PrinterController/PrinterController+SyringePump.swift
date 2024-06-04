263779362e6c11a25a865f5a8b5984ab555e966b
E-L-Cook <ecook@mines.edu>
2024-06-04 14:01:56 -0600

    changed setFlowRate functions to take doubles and convert them to strings to send

--- Fifi/ Controllers/PrinterController/PrinterController/PrinterController+SyringePump.swift
+++ Fifi/ Controllers/PrinterController/PrinterController/PrinterController+SyringePump.swift
@@ -9,16 +9,16 @@ import Foundation
 
 
 public extension PrinterController {
-    func setFlowRate1(to quantity: String, of units:String) async throws {
+    func setFlowRate1(to quantity: Double, of units:String) async throws {
         // TODO:
         try await with(.pump) {
-            try await syringePumpController?.setRate(for: "00", to: quantity, of: units)
+            try await syringePumpController?.setRate(for: "00", to: String(format: "%f", quantity), of: units)
         }
     }
 
-    func setFlowRate2(to quantity: String, of units:String) async throws {
+    func setFlowRate2(to quantity: Double, of units:String) async throws {
       try await with(.pump) {
-          try await syringePumpController?.setRate(for: "01", to: quantity, of: units)
+          try await syringePumpController?.setRate(for: "01", to: String(format: "%f", quantity), of: units)
       }
     }
     
