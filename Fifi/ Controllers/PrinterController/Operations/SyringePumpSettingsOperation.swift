3f0eec44ed273d5e59df313e2a04da7d5e917aed
E-L-Cook <ecook@mines.edu>
2024-06-04 11:47:27 -0600

    made modifications to make closer to final goal

--- Fifi/ Controllers/PrinterController/Operations/SyringePumpSettingsOperation.swift
+++ Fifi/ Controllers/PrinterController/Operations/SyringePumpSettingsOperation.swift
@@ -10,12 +10,14 @@ import SwiftUI
 public struct SyringePumpSettingsConfiguration: Hashable, Codable {
     // TODO: Add ON State
 //for each separate pump
-  public var flowRate1: Double?
-  public var flowRate2: Double?
+  public var flowRate1: String?
+  public var units1: String?
+  public var flowRate2: String?
+  public var units2: String?
   public var enable1: Bool?
   public var enable2: Bool?
-  public var id1: Double?
-  public var id2: Double?
+  public var id1: String?
+  public var id2: String?
     //TODO: set id1 and id2 are not used
     
   
@@ -33,25 +35,17 @@ extension PrinterOperation {
             configuration: .init(),
             name: "Syringe Pump Settings",
             thumbnailName: "syringe.fill", body: body) {configuration, printerController in
-                if let flowRate1 = configuration.flowRate1 {
+                if let flowRate1 = configuration.flowRate1, let units1 = configuration.units1 {
              //   TODO:
-                    try await printerController.setFlowRate1(to: flowRate1)
+                    try await printerController.setFlowRate1(to: flowRate1, of: units1)
                 }
                 
-                if let flowRate2 = configuration.flowRate2 {
-                    try await printerController.setFlowRate2(to: flowRate2)
+                if let flowRate2 = configuration.flowRate2, let units2 = configuration.units2 {
+                    try await printerController.setFlowRate2(to: flowRate2, of: units2)
                 }
                 
                 if let enable1 = configuration.enable1 {
                     try await printerController.enablePump1(to: enable1)
-                }
-                
-                if let enable2 = configuration.enable2 {
-                    try await printerController.enablePump2(to: enable2)
-                }
-                
-                if let  = configuration.enable2 {
-                    try await printerController.enablePump2(to: enable2)
                 }
                 
                 if let enable2 = configuration.enable2 {
