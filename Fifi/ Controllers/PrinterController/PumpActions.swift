1612c2377c83107cfd8396c3f4b83bfa887491d7
E-L-Cook <ecook@mines.edu>
2024-06-04 15:10:53 -0600

    added pump actions enum based on WaveFunction.swift

---
+++ Fifi/ Controllers/PrinterController/PumpActions.swift
@@ -0,0 +1,35 @@
+//
+//  PumpActions.swift
+//  Fifi
+//
+//  Created by Rafiq on 6/4/24.
+//
+
+import Foundation
+
+enum PumpAction: String, Codable, Hashable, CaseIterable {
+    
+    case start = "Start"
+    case stop = "Stop"
+    case hold = "Hold"
+    
+    public enum Error: Swift.Error {
+      case unknownFunction
+    }
+    
+    var queryString: String {
+        switch self {
+        case .start: return "Start"
+        case .stop: return "Stop"
+        case .hold: return "Hold"
+        }
+    }
+}
+
+#if canImport(SwiftUI)
+extension PumpAction: Identifiable {
+    public var id: String {
+        rawValue
+    }
+}
+#endif
